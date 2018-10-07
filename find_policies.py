968f83dfc8c7425512e9129258e04dd2b55490b5
Jonbo <jon.bosque.hernando@gmail.com>
2018-10-07 11:08:44 +0200

    Policy Iteration

--- find_policies.py
+++ find_policies.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from environment import *
 
 
@@ -20,7 +21,7 @@ class ValueIteration:
         penalty = environment.agent.penalty
         for _ in range(n_iterations):
             for state in self.utilty:
-                max_utility = -10000
+                max_utility = -math.inf
                 for action in actions:
                     new_state, new_reward = state.do_action(action)
                     utility = new_reward + penalty + discount * self.utilty[new_state]
@@ -29,9 +30,61 @@ class ValueIteration:
                 self.utilty[state] = max_utility
         return self.utilty
 
+
+class PolicyIteration:
+    def __init__(self, environment, utility=None, policy=None):
+        self.environment = environment
+        actions = self.environment.agent.actions
+        states = self.environment.states
+        if utility is None:
+            self.utilty = dict()
+            for state in environment.states:
+                rand = np.random.random()
+                self.utilty.update({state: rand})
+        else:
+            # check_environment_utility_are_compatible() UNIMPLEMENTED
+            self.utilty = utility
+        if policy is None:
+            self.policy = dict()
+            randint = np.random.randint(0, len(actions), size=len(states))
+            for state, i in zip(states, randint):
+                self.policy.update({state: actions[i]})
+        else:
+            self.policy = policy
+
+    def iterate(self, n_iterations):
+        states = self.environment.states
+        discount = self.environment.agent.discount
+        actions = self.environment.agent.actions
+        for _ in range(n_iterations):
+            # Evaluate Policy
+            for state in utility:
+                new_state, new_reward = state.do_action(self.policy[state])
+                utility[state] = new_reward + discount * utility[new_state]
+            # Improve Policy
+            for state in self.policy:
+                max_action = -math.inf
+                max_utility = -math.inf
+                for action in actions: # argmax( T(s,a,s') * U(s')
+                    new_state, new_reward = state.do_action(action)
+                    if max_utility < utility[new_state]:
+                        max_utility = utility[new_state]
+                        max_action = action
+                self.policy[state] = max_action
+        return self.policy
+
 if __name__ == '__main__':
-    environment = ClassicGridWorld(food=1, death=-10, penalty=-0.1, discount=0.9)
+    environment = ClassicGridWorld(food=1, death=-1, penalty=-0.1, discount=0.9)
+
+    print('Value Iteration: ')
     solver = ValueIteration(environment)
-    utility = solver.iterate(n_iterations=1000)
+    utility = solver.iterate(n_iterations=100)
     for state in utility:
         print('{0} : {1:8.2f}'.format(str(state), utility[state]))
+
+    print('\n\n')
+    print('Policy Iteration')
+    solver = PolicyIteration(environment)
+    policy = solver.iterate(n_iterations=100)
+    for state in policy:
+        print('{0} : {1:.2f} => {2}'.format(state, solver.utilty[state], policy[state]))
