b80ffde575be8eb4efaa5ddf09b4e8ffff3c1150
Jonbo <jon.bosque.hernando@gmail.com>
2019-02-14 10:41:27 +0100

    Better presentation. Add NonDeterministic case

--- TDAgent_use.py
+++ TDAgent_use.py
@@ -4,7 +4,8 @@ from td_learning import *
 if __name__ == '__main__':
     episode_length = 100
     learning_rate = 1 / episode_length ** 2
-    environment = env.ClassicGridWorld(food=10, death=-5, discount=0.9, penalty=-0.1)
+    environment = env.ClassicNonDeterministicGridWorld(food=1, death=-1, discount=0.9, penalty=-0.1)
+    # environment = env.ClassicGridWorld(food=1, death=-1, discount=0.9, penalty=-0.1)
     td_agent = TDAgent(0.7, learning_rate, environment)
     n_batch = 1000
     n_episode_batch = 100
@@ -15,7 +16,7 @@ if __name__ == '__main__':
             count += 1
         td_agent.process_episodes()
         episode = td_agent.play_greedy_episode(episode_length)
-        value_table_print(td_agent.value_table)
+        td_agent.print_policy()
         print('Greedy Episode: {0}  Reward: {1:4.2f}'.format(i, sum(episode.rewards)))
         episode.print_action_sequence()
         # input()
